92d1076550b2b330ac0c3ec49f741c2026a558c0
Castro Hamiltone <castromiltone@gmail.com>
2023-06-23 22:52:39 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts array of integers
+ * using bubble sort algorithm
+ *
+ * @array: an array of integers
+ * @size: the size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t k, l;
+	int tmp;
+
+	for (k = 0; k < size; k++)
+	{
+		for (l = 0; l < size - 1; l++)
+		{
+			if (array[l] > array[l + 1])
+			{
+				tmp = array[l];
+				array[l] = array[l + 1];
+				array[l + 1] = tmp;
+
+				print_array(array, size);
+			}
+		}
+	}
+}
