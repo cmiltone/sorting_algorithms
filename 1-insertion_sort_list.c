04ce87fc19540c58f357de64f50aeaea0b804691
Castro Hamiltone <castromiltone@gmail.com>
2023-06-25 21:46:30 +0300

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort linked list using insertion sort
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *new = NULL;
+	listint_t *current = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	new = *list;
+	new = new->next;
+
+	while (new)
+	{
+		while (new->prev && new->n < (new->prev)->n)
+		{
+			current = new;
+			if (new->next)
+				(new->next)->prev = current->prev;
+
+			(new->prev)->next = current->next;
+			new = new->prev;
+			current->next = new;
+			current->prev = new->prev;
+
+			if (new->prev)
+				(new->prev)->next = current;
+
+			new->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			print_list(*list);
+			new = new->prev;
+		}
+		new = new->next;
+	}
+
+}
--- sort.h
+++ sort.h
@@ -14,7 +14,7 @@
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
@@ -22,5 +22,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void insert(listint_t **head, listint_t *node);
 
 #endif
