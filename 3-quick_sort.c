d3790973ba46c5ba789149593ac2313c386b892a
Castro Hamiltone <castromiltone@gmail.com>
2023-06-26 18:18:57 +0300

    fix

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array on integers using quick sort algorithm
+ * @array: integer array
+ * @size: legnth of the array
+*/
+
+void quick_sort(int *array, size_t size)
+{	
+}
