6d0bf169a3e5ed16a95e006e7208605de4a24573
Castro Hamiltone <castromiltone@gmail.com>
2023-06-26 21:18:53 +0300

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,5 +7,65 @@
 */
 
 void quick_sort(int *array, size_t size)
-{	
+{
+	sort(array, 0, size - 1, size);
+}
+
+/**
+ * part - Lomuto partition
+ * @array: the array
+ * @low: integer
+ * @high: integer
+ * Return: integer
+*/
+
+int part(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, tmp;
+
+	for (j = low; j < high; j++)
+	{
+		if (pivot >= array[j])
+		{
+			i += 1;
+			if (array[i] != array[j])
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] != array[high])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * sort - performs sort recursively
+ * @array: the array of ints
+ * @low: int
+ * @hign: int
+ * Return: void
+*/
+
+void sort(int *array, int low, int high, size_t size)
+{
+	int partition;
+
+	if (low < high)
+	{
+		partition = part(array, low, high, size);
+		sort(array, low, partition - 1, size);
+		sort(array, partition + 1, high, size);
+	}
 }
